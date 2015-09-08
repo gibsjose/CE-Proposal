0c562b55e1ef1d49375492094ec88d5d2c8f1916
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-09-08 09:20:29 -0400

    Added Summary document

--- build.sh
+++ build.sh
@@ -2,15 +2,31 @@
 
 echo "Running pdflatex..."
 
-cd ./LaTeX/
+# Proposal
 # run twice to produce contents and resolve figs
+cd ./LaTeX/
 ret=$(pdflatex Proposal.tex < /dev/null)
 ret=$(pdflatex Proposal.tex < /dev/null)
 cd ..
 if [ -e ./LaTeX/Proposal.pdf ]
     then
     mv ./LaTeX/Proposal.pdf .
-    echo "Build complete"
+    echo "Build complete: Proposal.pdf"
+else
+    echo "[Error]: Error executing pdflatex:"
+    echo "$ret"
+fi
+
+# Summary
+# run twice to produce contents and resolve figs
+cd ./LaTeX/
+ret=$(pdflatex Summary.tex < /dev/null)
+ret=$(pdflatex Summary.tex < /dev/null)
+cd ..
+if [ -e ./LaTeX/Summary.pdf ]
+    then
+    mv ./LaTeX/Summary.pdf .
+    echo "Build complete: Summary.pdf"
 else
     echo "[Error]: Error executing pdflatex:"
     echo "$ret"
